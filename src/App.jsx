d0ee2a3d0cedd6d0bd46d500d148e67837179075
Emmyojile <emmaojile99@gmail.com>
2023-09-29 22:11:23 -0800

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import useMediaQuery from "./hooks/useMediaQuery";
 import Navbar from "./components/Navbar";
 import DotGroup from "./components/DotGroup";
+import Landing from "./components/Landing";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState("home");
@@ -26,10 +27,13 @@ function App() {
           setSelectedPage={setSelectedPage}
         />
         <div className="w-5/6 mx-auto md:h-full">
-          <DotGroup
+          {isAboveMediumScreens && (
+            <DotGroup
             selectedPage={selectedPage}
             setSelectedPage={setSelectedPage}
           />
+          )}
+          <Landing setSelectedPage={setSelectedPage}/> 
         </div>
       </div>
     </>
